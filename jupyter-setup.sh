4a35bb9223b466023cc9d3045bc7b6bc1a5c376c
darrida <ben.Davis.hammond@gmail.com>
2018-11-25 18:18:51 -0600

    Add files via upload

---
+++ jupyter-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#Purpose of script:
+#  -Built for use with UserLAnd Android app on a Chromebook
+#   (probably works fine on Android)
+#  -If not downloaded for this purpose, probably as a result of
+#   reading the guide, please see the guid for more information:
+#    -URL:
+##############################################
+sudo apt-get --assume-yes install git
+sudo apt-get --assume-yes install openssh-server
+sudo apt-get --assume-yes install python3
+sudo apt-get --assume-yes remove --auto-remove python-pip
+sudo apt-get --assume-yes install python3-pip
+sudo python3 -m pip install --upgrade pip
+sudo pip install jupyter
+sudo pip install jupyterlab
+sudo pip install pandas
+sudo pip install bs4
+sudo apt-get --assume-yes install pkg-config
+sudo apt-get --assume-yes install libfreetype6-dev
+sudo pip install matplotlib
+echo ""
+echo "JUPYTER SHOULD BE READY TO USE NOW."
+echo ""
+echo "The following were installed: git, openssh-server, python3, jupyter, jupyterlab, pandas, bs4, matplotlib."
+##############################################
+#Contact: darrida | darrida.py@gmail.com | tech.theogeek.com
