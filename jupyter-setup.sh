e2ebb1c9de40290c88d4e503861d9924184e7cdb
benh <bhammond@waubonsee.edu>
2018-11-28 13:14:34 -0600

    Update jupyter-setup.sh

--- jupyter-setup.sh
+++ jupyter-setup.sh
@@ -3,8 +3,7 @@
 #Purpose of script:
 #  -Built for use with UserLAnd Android app on a Chromebook
 #   (probably works fine on Android)
-#  -If not downloaded for this purpose, probably as a result of
-#   reading the guide, please see the guid for more information:
+#  -Please see the guid for more information:
 #    -URL: https://github.com/darrida/chromeos_jupyter_for_userland
 ##############################################
 sudo apt-get --assume-yes install git
